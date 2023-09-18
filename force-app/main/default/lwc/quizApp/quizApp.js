10d0591feb24f2ea15ecca58b3154f73a5c03848
sridharan15-lab <sridharan2000babu@gmail.com>
2023-09-18 23:43:53 +0530

    third commit

--- force-app/main/default/lwc/quizApp/quizApp.js
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -1,3 +1,77 @@
 import { LightningElement } from 'lwc';
 
-export default class QuizApp extends LightningElement {}
+export default class QuizApp extends LightningElement {
+
+    selected={} //storing valuees
+    correctAnswers=0 //show correct ans count
+
+    myQuestions=[
+        {
+            id:"Question1",
+            question:"Developer Tools required to work on LWC?",
+            answers:{
+                a:"Eclipse",
+                b:"Developer Console",
+                c:"VS Code",
+                d:"Workbench"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question2",
+            question:"What’s the annotation used to invoke an Apex method imperatively from LWC?",
+            answers:{
+                a:"@future",
+                b:"@isTest",
+                c:"@AuraEnabled",
+                d:"@cacheable"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id:"Question3",
+            question:"What’s the annotation used to wire an Apex method to a property or function in LWC?",
+            answers:{
+                a:"@future",
+                b:"@AuraEnabled(cacheable=true)",
+                c:"@AuraEnabled",
+                d:"@cacheable"
+            },
+            correctAnswer:"b"
+        },
+        {
+            id:"Question4",
+            question:"What are the maximum no of components that we can include in an LWC?",
+            answers:{
+                a:"9",
+                b:"15",
+                c:"100",
+                d:"No Limit"
+            },
+            correctAnswer:"d"
+        }
+
+    ]
+
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+    }
+
+    changeHandler(event){
+        console.log("name",event.target.name)
+        console.log("value",event.target.value)
+
+        const{name,value}=event.target //explained below
+        //const name = event.target.name
+        //const value = event.target.value
+        this.selected = {...this.selected, [name]:value}
+        //this.selected={"question1":"a"}
+    }
+    submitHandler(event){
+        
+
+    }
+    resetHandler(event){
+
+    }
+}
